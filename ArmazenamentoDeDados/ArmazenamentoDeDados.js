86e886500844f0c001da9302f862cce6249f9b7d
Matheu <matheuge@gmail.com>
2020-06-19 15:29:04 -0300

    App

--- ArmazenamentoDeDados/ArmazenamentoDeDados.js
+++ ArmazenamentoDeDados/ArmazenamentoDeDados.js
@@ -1,31 +1,47 @@
+var dados = null;
+var state = 'list';
+
 function criar(nome, idade){ 
-     
+  dados.push({"id":dados.length+1,"nome":nome, "idade":idade});
+  print_users();
 }
 
-function editar(id, nome, idade){
-    TodosDados[id].nome = nome;
-    TodosDados[id].idade = idade;
-}
-
-function excluir(id){
-    modificando = TodosDados[id]
-
-}
-
-function consultar(){
-    return TodosDados;
+function campos(){
+  if(state == 'list'){
+    state = 'create';
+    $('.users').html("<input class='name' type='text' placeholder='Nome'><br><input class='age' type='text' placeholder='Idade'>");
+  }else{
+    state = 'list';
+    var name = $('.name').val();
+    var age = $('.age').val();
+    criar(name, age);
+  }
 }
 
 function load(){
-    $.ajax({
-        url: "https://fiddle.jshell.net/favicon.png",
-        beforeSend: function( xhr ) {
-          xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
-        }
-      })
-        .done(function( data ) {
-          if ( console && console.log ) {
-            console.log( "Sample of data:", data.slice( 0, 100 ) );
-          }
-        });
+  // $.ajax({
+  //   type: 'POST',
+  //   dataType: 'json',
+  //   data: {'texto': 'texto_bug'},
+  //   url: '../ArmazenamentoDeDados/Dados.json',
+  //   success: function(data) {
+  //     dados = data;
+  //     print_users();
+  //   }
+  // });
+  dados = [
+    {"id": 1, "nome" : "Matheu", "idade" : "20"},
+    {"id": 2, "nome" : "Alex", "idade" : "23"},
+    {"id": 3, "nome" : "Igor", "idade" : "30"},
+    {"id": 4, "nome" : "Antonio", "idade" : "45"}
+   ];
+  print_users();
+}
+
+function print_users(){
+  $('.users').html("<table class=\"tabela\"></table>");
+  $('.tabela').append("<tr><td>ID</td><td>Nome</td><td>Idade</td></tr>");
+  for(i in dados){
+    $('.tabela').append("<tr><td>"+dados[i]["id"]+"</td><td>"+dados[i]["nome"]+"</td><td>"+dados[i]["idade"]+"</td></tr>");
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # monorepository
+
+Um exemplo simples de mono-repositório utilizando html e javascript.
+
+Não é necessário a instalação de nenhum adicional, apenas clonar o repositório.
+
